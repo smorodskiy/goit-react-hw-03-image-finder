c9e5eb70cc9100558758bbb4c3effad6916960ce
Igor Smorodsky <ingerard@gmail.com>
2022-11-04 23:12:40 +0200

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-export * from './Button/Button';
+export * from './LoadMoreButton/LoadMoreButton';
 export * from './ImageGallery/ImageGallery';
 export * from './ImageGalleryItem/ImageGalleryItem';
 export * from './Loader/Loader';

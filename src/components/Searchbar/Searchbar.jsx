fa9a9e621a11074d091f0f225191b601ba41fdee
Igor Smorodsky <ingerard@gmail.com>
2022-11-04 14:14:31 +0200

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -3,22 +3,37 @@ import { Component } from 'react';
 
 // Check types of props
 import PropTypes from 'prop-types';
+import {
+  SearchHeader,
+  SearchForm,
+  SearchButton,
+  SearchButtonLabel,
+  SearchInput,
+} from './Searchbar.styled';
 
 class Searchbar extends Component {
   render() {
-    return(<></>)
+    return (
+      <SearchHeader class="searchbar">
+        <SearchForm class="form">
+          <SearchButton type="submit" class="button">
+            <SearchButtonLabel class="button-label">Search</SearchButtonLabel>
+          </SearchButton>
+
+          <SearchInput
+            class="input"
+            type="text"
+            autocomplete="off"
+            autofocus
+            placeholder="Search images and photos"
+          />
+        </SearchForm>
+      </SearchHeader>
+    );
   }
 }
 
 export { Searchbar };
 
 // Types
-Searchbar.propTypes = {
-  Searchbar: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-    })
-  ),
-  onDeleteUser: PropTypes.func.isRequired,
-};
+Searchbar.propTypes = {};
